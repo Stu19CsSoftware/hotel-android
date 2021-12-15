0cf075ace4f9a1c0093e14e0da70df5b20d6ebc5
ranlychan <513446825@qq.com>
2021-12-15 21:10:34 +0800

    @Ranly 1.客房详情页layout 2.我的页面UI调整 3.引入一些第三包依赖包

--- app/src/main/java/com/ranlychan/hotel/activity/RoomDetailActivityMain.java
+++ app/src/main/java/com/ranlychan/hotel/activity/RoomDetailActivityMain.java
@@ -4,14 +4,44 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
+import com.hjq.bar.OnTitleBarListener;
+import com.hjq.bar.TitleBar;
 import com.ranlychan.hotel.R;
 
 public class RoomDetailActivityMain extends BaseActivity {
+    private TitleBar titleBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room_detail_main);
+        initView();
+        intiTitleBar();
+    }
+
+    private void initView(){
+        titleBar = findViewById(R.id.tb_room_detail);
+    }
+
+    private void intiTitleBar(){
+        titleBar.setOnTitleBarListener(new OnTitleBarListener() {
+            @Override
+            public void onLeftClick(TitleBar titleBar) {
+                finish();
+            }
+
+            @Override
+            public void onTitleClick(TitleBar titleBar) {
+
+            }
+
+            @Override
+            public void onRightClick(TitleBar titleBar) {
+
+            }
+        });
     }
 }
